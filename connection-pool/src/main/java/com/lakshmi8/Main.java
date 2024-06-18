115bc16167144ec043fb4ee365b064e136a15a5c
Lakshmi Narayanan N V <nv.karthick8@gmail.com>
2024-06-18 22:42:41 +0530

    Connection pool prototype initial version

---
+++ connection-pool/src/main/java/com/lakshmi8/Main.java
@@ -0,0 +1,63 @@
+package com.lakshmi8;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    private static Connection createNewConnection() {
+        String jdbcUrl =  "jdbc:mysql://localhost:3306/prototype_db";
+        String username = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        if (username == null || password == null) {
+            System.err.println("Database credentials are not set in the environment");
+            return null;
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+            System.out.println("Returning a connection object");
+            return connection;
+        } catch (SQLException sqlException) {
+            System.err.println("Connection could not be established");
+            sqlException.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        int numThreads = 150; // Found out that only 150 connections are supported at max
+        for (int i = 0; i < numThreads; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Connection connection = null;
+                    try {
+                        connection = createNewConnection();
+                        if (connection == null) {
+                            System.err.println(Thread.currentThread().getName() + " could not establish a connection.");
+                            return;
+                        }
+                        Thread.sleep(60000);
+                        System.out.println(Thread.currentThread().getName() + " has finished its task");
+
+                    } catch (InterruptedException interruptedException) {
+                        System.err.println(Thread.currentThread().getName() + " was interrupted.");
+                        interruptedException.printStackTrace();
+                    } finally {
+                        if (connection != null) {
+                            try {
+                                connection.close();
+                                System.out.println(Thread.currentThread().getName() + " closed the connection.");
+                            } catch (SQLException sqlException) {
+                                sqlException.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }, "Thread-" + (i + 1)).start();
+        }
+    }
+}
